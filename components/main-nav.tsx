d66519a024a7f40bfa61a88cdca141c1f52bf092
youssef <youssef1362009kamel@gmail.com>
2026-03-13 21:22:04 +0200

    fix: extreme mobile overflow fix

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -22,7 +22,7 @@ const MainNav: React.FC<MainNavProps> = ({
 
     return (
         <nav
-            className="mx-6 hidden lg:flex items-center space-x-4 lg:space-x-6 shrink-0"
+            className="mx-6 hidden xl:flex items-center space-x-4 lg:space-x-6 shrink-0"
         >
             {routes.map((route) => (
                 <Link
