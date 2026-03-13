e2c9185a5ed994637ea7585cce243d3244edbbb3
youssef <youssef1362009kamel@gmail.com>
2026-03-13 21:19:00 +0200

    fix: aggressive mobile nav and overflow kill switch

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -22,7 +22,7 @@ const MainNav: React.FC<MainNavProps> = ({
 
     return (
         <nav
-            className="mx-6 hidden md:flex items-center space-x-4 lg:space-x-6"
+            className="mx-6 hidden lg:flex items-center space-x-4 lg:space-x-6 shrink-0"
         >
             {routes.map((route) => (
                 <Link
--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -29,7 +29,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ data }) => {
 
     return (
         <>
-            <div className="flex items-center md:hidden mr-2">
+            <div className="flex items-center lg:hidden mr-2">
                 <IconButton 
                     onClick={onOpen}
                     icon={<Menu size={20} />}
@@ -37,7 +37,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ data }) => {
                 />
             </div>
 
-            <Dialog open={open} as="div" className="relative z-50 md:hidden" onClose={onClose}>
+            <Dialog open={open} as="div" className="relative z-50 lg:hidden" onClose={onClose}>
                 {/* Background overlay */}
                 <div className="fixed inset-0 bg-black bg-opacity-25" />
 
